fcca07a8508449a98ac309503e079cca49919459
yoshida <yoshida@ymba.local>
2016-05-09 21:34:06 +0900

    L1: Type Ctrl/Shift keys with thumbs / Type Command/L1 with little fingers

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -20,18 +20,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | ~L1  |           | ~L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Alt  |           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | Cmd    |A / L2|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
+ * |--------+------+------+------+------+------| Alt  |           | Alt  |------+------+------+------+------+--------|
+ * | ~L1    |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |    ~L1 |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv   |  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |Grv   |      |AltShf| LAlt | LCtrl|                                       | RCtrl| RAlt |   [  |   ]  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  |IMETGL|       | Alt  |Ctrl/Esc|
+ *                                        | App  |IMETGL|       | Alt  | Esc  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 |      |      |      |       |      |        |      |
  *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 |      |ace   |LShft |       | RShft|        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -40,21 +40,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_TRNS,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   MO(SYMB),
-        KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LALT,
-        KC_GRV,         KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+        KC_LGUI,        LT(MDIA,KC_A),KC_S,   KC_D,   KC_F,   KC_G,
+        MO(SYMB),       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LALT,
+        KC_GRV,         KC_TRNS,      LALT(KC_LSFT),  KC_LALT,KC_LCTRL,
                                               ALT_T(KC_APP),  LSFT(LGUI(KC_SPC)),
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+                                                              KC_TRNS,
+                                              KC_SPC,KC_BSPC, KC_LSFT,
         // right hand
              KC_TRNS,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              MO(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          MO(SYMB),
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+             KC_RALT,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          MO(SYMB),
+                                  KC_RCTRL,KC_LALT,KC_LBRC,KC_RBRC,        KC_TRNS,
+             KC_LALT,     KC_ESC,
+             KC_TRNS,
+             KC_RSFT, KC_TAB, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
